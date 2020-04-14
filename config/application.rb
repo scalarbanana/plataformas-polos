d79d0f803862c56b07cfc77681276c17d76fda0f
Ariel Santos <ariel.santos@dcc.ufmg.br>
2020-04-14 17:48:06 -0300

    Ajustando views de formulário

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module PlataformasPolos
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.i18n.default_locale = :'pt-BR'
+    config.i18n.available_locales = 'pt-BR'
+    config.i18n.default_locale = 'pt-BR'
 
     config.generators.stylesheets = false
     config.generators.javascripts = false
